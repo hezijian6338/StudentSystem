3910ca2f9eaf1086eba9f06190b06e009c45f7d5
hezijian6338 <627222344@qq.com>
2018-11-26 09:00:02 +0800

    取消跨域，改在nginx上处理

--- src/main/java/com/Mrs/Wang/project/configurer/WebMvcConfigurer.java
+++ src/main/java/com/Mrs/Wang/project/configurer/WebMvcConfigurer.java
@@ -102,11 +102,11 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
-                .maxAge(3600)
-                .allowCredentials(true);
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+//                .maxAge(3600)
+//                .allowCredentials(true);
     }
 
     //添加拦截器
