72f871455dce168213778c5caa297a83359fb0de
hezijian6338 <627222344@qq.com>
2018-11-26 08:37:40 +0800

    修改跨域代码，尝试全局跨域处理

--- src/main/java/com/Mrs/Wang/project/configurer/WebMvcConfigurer.java
+++ src/main/java/com/Mrs/Wang/project/configurer/WebMvcConfigurer.java
@@ -103,6 +103,7 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**")
+                .allowedOrigins("*")
                 .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
                 .maxAge(3600)
                 .allowCredentials(true);
